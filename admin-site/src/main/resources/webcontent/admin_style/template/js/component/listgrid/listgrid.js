89dfeb788be9934e1ac58c953d89d09fa1d1a8f9
Gao Yuan <andy.gao.y@gmail.com>
2015-06-30 22:55:10 +0800

    ListGrid update

--- admin-site/src/main/resources/webcontent/admin_style/template/js/component/listgrid/listgrid.js
+++ admin-site/src/main/resources/webcontent/admin_style/template/js/component/listgrid/listgrid.js
@@ -11,7 +11,12 @@
         var LISTGRID_HEADER_TABLE = "div.listgrid-header-wrapper > table";
         var LISTGRID_BODY = "div.listgrid-body-wrapper";
         var LISTGRID_BODY_TABLE = "div.listgrid-body-wrapper table";
-        var LISTGRID_FOOTER = "div.listgrid-footer-wrapper"
+        var LISTGRID_FOOTER = "div.listgrid-footer-wrapper";
+
+        var LOAD_SPINNER = 'i.listgrid-table-spinner';
+
+        var fetchDebounce = 200;
+        var lockDebounce = 200;
 
         var tableColumnResizing = {
             active : false,
@@ -24,9 +29,198 @@
             totalWidth : 0
         };
 
+        var Range = function (rangeDesc) {
+            if(arguments.length == 2){
+                this.lo = arguments[0];
+                this.hi = arguments[1];
+            }else if (typeof rangeDesc == 'string') {
+                var range = rangeDesc.split('-');
+                this.lo = parseInt(range[0]);
+                this.hi = parseInt(range[1]);
+            }else if(typeof rangeDesc == 'object'){
+                this.lo = rangeDesc.lo;
+                this.hi = rangeDesc.hi;
+            }
+        };
+
+        Range.prototype = {
+                toString : function(){
+                        return this.lo + '-' + this.hi;
+                },
+                clone :function(){
+                  return new Range(this.lo, this.hi);      
+                },
+            compareIndex : function(index){
+                return (index < this.lo) ? -1 : ( index >= this.hi ? 1 : 0);
+            },
+            containsIndex : function(index){
+                return ((this.lo <= index) && (index < this.hi));
+            },
+            overlap : function(range){
+                var a,b;
+                (this.lo < range.lo) ? (a = this, b = range) : (a = range, b=this);
+                return (b.lo < a.hi);
+            },
+            merge : function(range){
+                var a,b;
+                (this.lo < range.lo) ? (a = this, b = range) : (a = range, b=this);
+                return (b.lo <= a.hi) ? (new Range(a.lo, Math.max(a.hi, b.hi))) : null;
+            },
+            findGap : function(range){
+                var a,b;
+                (this.lo < range.lo) ? (a = this, b = range) : (a = range, b=this);
+                return (b.lo > a.hi) ? (new Range(a.hi, b.lo)) : null;
+            },
+            width : function(){
+                    return this.hi - this.lo;
+            },
+            intersect : function (range) {
+                var a,b;
+                (this.lo < range.lo) ? (a = this, b = range) : (a = range, b=this);
+                return (b.lo < a.hi) ? (new Range(Math.max(a.lo, b.lo), Math.min(a.hi,b.hi))) : null;
+            },
+            drop : function(range, withempty) {
+                if (withempty == undefined) {
+                    withempty = false;
+                }
+                var result = [];
+                if (range.hi <= this.lo) {
+                    if (withempty) {
+                        result.push(null);
+                    }
+                    result.push(this.clone());
+                } else if (this.hi <= range.lo) {
+                    result.push(this.clone());
+                    if (withempty) {
+                        result.push(null);
+                    }
+                } else if (range.lo <= this.lo) {
+                    if (range.hi < this.hi) {
+                        if (withempty) {
+                            result.push(null);
+                        }
+                        result.push(new Range(range.hi, this.hi));
+                    } else if (withempty) {
+                        result.push(null);
+                        result.push(null);
+                    }
+                } else if (this.hi <= range.hi) {
+                    if (this.lo < range.lo) {
+                        result.push(new Range(this.lo, range.lo));
+                        if (withempty) {
+                            result.push(null);
+                        }
+                    } else if (withempty) {
+                        result.push(null);
+                        result.push(null);
+                    }
+                } else if (this.lo < range.lo && range.hi < this.hi) {
+                    result.push(new Range(this.lo, range.lo));
+                    result.push(new Range(range.hi, this.hi));
+                } else {
+                    null.null; //assert faill
+                }
+                return result;
+            },
+            chipOffOnePiece : function (pieceWidth, fromTail) {
+                if(fromTail == undefined){
+                    fromTail = false;
+                }
+                return fromTail ? (new Range(this.hi - pieceWidth, this.hi)) : (new Range(this.lo, this.lo + pieceWidth));
+            }
+        };
+
+        var Ranges = {
+            addRange : function(ranges, range){
+                ranges.push(range);
+                ranges.sort(function(a,b){return a.lo - b.lo});
+            },
+            containsIndex:function(ranges, index){
+                return ranges.some(function(item, arrayIndex, array){
+                    return item.containsIndex(index);
+                });
+            },
+            merge : function (ranges) {
+                var result = [];
+                ranges.forEach(function (item, index, array) {
+                    if(result.length == 0){
+                        result.push(item);
+                    }else{
+                        var last = result.pop();
+                        var merged = last.merge(item);
+                        merged ? result.push(merged) : (result.push(last), result.push(item));
+                    }
+                });
+                return result;
+            },
+            intersect : function(ranges, range){
+                var result = [];
+                ranges.forEach(function(item, index, array){
+                    var ri = item.intersect(range);
+                    if(ri){
+                        result.push(ri);
+                    }
+                });
+                return result;
+            },
+            findMissingRangesWithin : function (ranges, from, to) {
+                var mainRange = new Range(from, to);
+                var intersects = this.intersect(ranges, mainRange);
+                var result = [];
+                var lastEnd = from;
+                intersects.forEach(function (item, index, array) {
+                    if(item.lo > lastEnd){
+                        result.push(new Range(lastEnd, item.lo));
+                    }
+                    lastEnd = item.hi;
+                });
+                if(lastEnd < to){
+                    result.push(new Range(lastEnd, to));
+                }
+                return result;
+            },
+            findMissingRanges : function (ranges, from, to, totalCount) {
+                var mainRange = new Range(from, to);
+                var result = [];
+                var lastEnd = 0;
+                var itemBehind = null;
+                var finish = false;
+                for(var i = 0 ; i < ranges.length ; ++i){
+                    var item = ranges[i];
+                    if(from < item.lo){
+                        result.push(new Range(lastEnd, item.lo));
+                    }
+
+                    lastEnd = item.hi;
+                    if( to <= item.lo){
+                        finish = true;
+                        break;
+                    }
+                }
+                if(!finish){
+                    result.push(new Range(lastEnd, totalCount))
+                }
+                return result;
+            },
+            makePageRanges : function (ranges, pageSize){
+                    var result = [];
+                if(ranges.length == 0){
+                    return null;
+                }
+                var rangesCount = ranges.length;
+                for(var i = 0 ; i < rangesCount ; ++i){
+                        var range = ranges[i];
+                        var t = range.slice(pageSize, (i == 0));
+                        result.concat(t);
+                }
+                return result;
+            }
+        }
+
         var ListGrid = function (container){
             this.$container = $(container);
             this.initialize();
+            this.paging.triggerLoad(this.$container);
         };
 
         ListGrid.prototype = {
@@ -82,6 +276,8 @@
                     bodyWrapper.customScrollbar({
                         onCustomScroll: function(event, scrollData) {
                             $this.paging.updateTableFooter($this.$container.find(LISTGRID_BODY_TABLE + ' tbody'));
+                            $this.paging.triggerLoad($this.$container);
+
                         }
                     });
                     this.updateHeaderWidth();
@@ -100,40 +296,63 @@
             }
         };
 
-
         var Paging = function(listGrid){
             this.listGrid = listGrid;
             this.$container = listGrid.$container;
-
+            this.LISTGRID_AJAX_LOCK = 0;
         };
 
         Paging.prototype = {
-            // ************************* *
-            // UTILITY *
-            // ************************* *
-            getRange : function(rangeDescription) {
-                var range = rangeDescription.split('-');
-                var rangeObj = {lo : parseInt(range[0]), hi : parseInt(range[1])};
-                return rangeObj;
+            // ********************** *
+            // LOCK RELATED FUNCTIONS *
+            // ********************** *
+
+            acquireLock : function() {
+                if (this.LISTGRID_AJAX_LOCK == 0) {
+                    this.LISTGRID_AJAX_LOCK = 1;
+                    return true;
+                }
+                return false;
+            },
+
+            releaseLock : function() {
+                this.LISTGRID_AJAX_LOCK = 0;
             },
 
             // ************************* *
             // entity data method *
             // ************************* *
+            getBaseUrl : function ($tbody) {
+                return $tbody.data('baseurl');
+            },
+            getLoadParameter : function($tbody){
+                return $tbody.data('currentparameter');
+            },
             getTotalRecords : function($tbody){
-                var $container = $tbody.closest(LISTGRID_CONTAINER);
-                return $container.data('totalrecords');
+                return $tbody.data('totalrecords');
             },
 
-            getLoadedRecordRange : function($container){
-                var rangeDescriptions = $container.data('recordranges').split(',');
+            getLoadedRecordRanges : function($tbody){
+                var rangeDescriptions = $tbody.data('recordranges').split(',');
                 var ranges = [];
 
-                for (var i = 0; i < rangeDescriptions.length; i++) {
-                    ranges[i] = this.getRange(rangeDescriptions[i]);
-                }
+                rangeDescriptions.forEach(function (item, index, array) {
+                    Ranges.addRange(ranges, new Range(item));
+                })
 
                 return ranges;
+            },
+            addLoadedRecordRange : function ($tbody, range) {
+                var ranges = this.getLoadedRecordRanges($tbody);
+                console.trace("range change from: " + ranges);
+                Ranges.addRange(ranges, range);
+                ranges = Ranges.merge(ranges);
+                var rangesString = ranges.join(',');
+                console.trace("range change to: " + rangesString);
+                $tbody.data('recordranges', rangesString);
+            },
+            getPageSize : function($tbody) {
+                return $tbody.data('pagesize');
             },
 
             // ************************* *
@@ -182,6 +401,22 @@
                 bodyWrapper.customScrollbar("scrollToY", offset);
             },
 
+            showLoadingSpinner : function ($tbody, spinnerOffset) {
+                var $spinner = $tbody.closest(LISTGRID_CONTAINER).find(LOAD_SPINNER);
+
+                if (spinnerOffset) {
+                    $spinner.css('position', 'absolute').css('top', spinnerOffset + 'px');
+                }
+
+                $spinner.parent().css('display', 'block');
+            },
+
+            hideLoadingSpinner : function($tbody) {
+                var $spinner = $tbody.closest(LISTGRID_CONTAINER).find(LOAD_SPINNER);
+                $spinner.parent().css('display', 'none');
+            },
+
+
             updateTableFooter : function($tbody){
                 var topIndex = this.getTopVisibleIndex($tbody);
                 var bottomIndex = this.getBottomVisibleIndex($tbody);
@@ -193,6 +428,115 @@
                 $footer.find('.total-records').text(totalCount);
             },
 
+            loadRecords : function($container){
+                while (!this.acquireLock()) {
+                    var _this = this;
+                    //console.log("Couldn't acquire lock. Will try again in " + lockDebounce + "ms");
+                    $.doTimeout('acquirelock', lockDebounce, function() {
+                        _this.loadRecords($container);
+                    });
+                    return false;
+                }
+
+                var $tbody = $container.find(LISTGRID_BODY_TABLE + ' tbody');
+                // If we can't see the list grid at all, don't load anything
+                if (!$tbody.is(':visible')) {
+                    this.releaseLock();
+                    return false;
+                }
+
+                var topIndex = this.getTopVisibleIndex($tbody);
+                var botIndex = this.getBottomVisibleIndex($tbody);
+                var dataWindowRange = new Range(topIndex, botIndex);
+                var ranges = this.getLoadedRecordRanges($tbody);
+                var pageSize = this.getPageSize($tbody);
+                var totalRecords = this.getTotalRecords($tbody);
+
+                var topIndexLoaded = Ranges.containsIndex(ranges,topIndex);
+                var botIndexLoaded = Ranges.containsIndex(ranges,botIndex);
+
+                var missingRanges = Ranges.findMissingRangesWithin(ranges,topIndex, botIndex);
+                if(missingRanges.length > 0){
+                    var baseUrl = this.getBaseUrl($tbody);
+                    baseUrl = window.location.origin + '/' +baseUrl;
+                    var firstMissingRange = missingRanges[0];
+                    firstMissingRange = firstMissingRange.chipOffOnePiece(pageSize, (firstMissingRange.lo == topIndex));
+                    var loadingWindowRange = dataWindowRange.intersect(firstMissingRange);
+                    var spinnerOffset = ((loadingWindowRange.lo + loadingWindowRange.hi) / 2 - topIndex) * this.getRowHeight($tbody);
+
+                    this.showLoadingSpinner($tbody, spinnerOffset);
+
+                    this.ajaxLoadMissing($tbody, baseUrl, firstMissingRange);
+                }
+
+                var startIndex = null;
+                var maxIndex = null;
+                this.releaseLock();
+            },
+
+            // ************************* *
+            // AJAX *
+            // ************************* *
+            triggerLoad : function($container){
+                if($container == undefined){
+                    $container = this.$container;
+                }
+                var $this = this;
+                $.doTimeout('fetch', fetchDebounce, function(){
+                    $this.loadRecords($container);
+                });
+            },
+            ajaxLoadMissing : function ($tbody, baseUrl, range) {
+                    var url = tallybook.history.getUrlWithParameter('startIndex', range.lo, null, baseUrl);
+                    url = tallybook.history.getUrlWithParameter('pageSize', range.width(), null, url);
+                    var $paging = this;
+                    $.ajax({url:url, success:function(data){
+                        var $data = $(data);
+                        var $newTbody = $data.find('tbody');
+                        $paging.injectRecords($tbody, $newTbody);
+                        $paging.releaseLock();
+                        $paging.hideLoadingSpinner($tbody);
+                        $paging.triggerLoad();
+                    }});
+            },
+            // ************************* *
+            // DOM *
+            // ************************* *
+            injectRecords : function($tbody, $newTbody){
+                var _this = this;
+                var newRange = this.getLoadedRecordRanges($newTbody)[0];
+                $tbody.find('tr.blank-padding').each(function(index, element){
+                        var $e = $(element);
+                        var range = new Range($e.data('range'));
+                        var intersect = range.intersect(newRange);
+                        if(intersect != null){
+                            var blanks = range.drop(intersect, true);
+                            var preblank = blanks[0];
+                            var posblank = blanks[1];
+                            var $prepad = null;
+                            var $pospad = null;
+                            // Extract the new rows
+                            var $newTrs = $newTbody.find('tr');
+                            if(preblank){
+                                $prepad = _this.createPadding($tbody, preblank.lo, preblank.hi);
+                                $newTrs.splice(0,0,$prepad[0]);
+                            }
+                            if(posblank){
+                                $pospad = _this.createPadding($tbody, posblank.lo, posblank.hi);
+                                $newTrs.push($pospad[0]);
+                            }
+                            $e.replaceWith($newTrs);
+                            _this.addLoadedRecordRange($tbody, intersect);
+                            
+
+                            // Extract the new rows
+                            
+                            //$newTrs
+                           // this.createPadding()
+
+                        }
+                });
+            },
 
             // ************************* *
             // Initialize *
@@ -217,7 +561,7 @@
 
                 var $tbody = bodyTable.find('tbody');
 
-                var range = this.getLoadedRecordRange(this.$container)[0];
+                var range = this.getLoadedRecordRanges($tbody)[0];
                 var recordsAbove = range.lo;
                 var recordsBelow = this.getTotalRecords($tbody) - range.hi;
                 var rowHeight = this.getRowHeight($tbody);
@@ -324,9 +668,13 @@
             var listGrid = new ListGrid($(this));
             $(document).ready(function () {
                 $(window).resize(function(){
-                    setTimeout(function () {
+                    $.doTimeout( 'resize', 250, function(){
                         listGrid.resize();
-                    }, 200);
+                    });
+  /*                  setTimeout(function () {
+                        listGrid.resize();
+                    }, 250);
+                    */
                 });
             });
 
--- admin-site/src/main/resources/webcontent/admin_style/template/js/tallybook.js
+++ admin-site/src/main/resources/webcontent/admin_style/template/js/tallybook.js
@@ -9,8 +9,72 @@ var tallybook = (function($){
     var requestParameterSortAsc="asc";
     var requestParameterSortDesc="desc";
 
-    return {
+    var history ={
+        getUrlWithParameter : function(param, value, state, baseUrl) {
+            if (baseUrl == null) {
+                baseUrl = window.location.href;
+            }
 
+            var indexOfQ = baseUrl.indexOf('?');
+            var urlParams = null;
+            if (indexOfQ >= 0) {
+                urlParams = baseUrl.substring(indexOfQ + 1);
+                baseUrl = baseUrl.substring(0, indexOfQ);
+            }
+
+            // Parse the current url parameters into an object
+            var paramObj = {};
+            if (urlParams != null) {
+                paramObj = JSON.parse('{"'
+                + decodeURI(encodeURI(urlParams.replace(/&/g, "\",\"").replace(/=/g,"\":\""))) + '"}');
+
+            }
+
+            if (value == null || value === "") {
+                delete paramObj[param];
+            } else {
+                // Update the desired parameter to its new value
+                if ($.isArray(param)) {
+                    $(param).each(function(index, param) {
+                        paramObj[param[index]] = value[index];
+                    });
+                } else {
+                    paramObj[param] = value;
+                }
+            }
+
+            // Reassemble the new url
+            var newUrl = baseUrl + '?';
+            for (i in paramObj) {
+                if (paramObj[i] != null) {
+                    newUrl += i + '=' + paramObj[i] + '&';
+                }
+            }
+            newUrl = newUrl.substring(0, newUrl.length-1);
+
+            return newUrl;
+        },
+
+        getUrlParameters : function() {
+            var baseUrl = window.location.href;
+            var indexOfQ = baseUrl.indexOf('?');
+            var urlParams = null;
+            if (indexOfQ >= 0) {
+                urlParams = baseUrl.substring(indexOfQ + 1);
+                return JSON.parse('{"'
+                + decodeURI(encodeURI(urlParams.replace(/&/g, "\",\"").replace(/=/g,"\":\""))) + '"}');
+            }
+            return null;
+        },
+
+        replaceUrlParameter : function(param, value, state) {
+            var newUrl = this.getUrlWithParameter(param, value, state);
+            this.replaceUrl(newUrl, state);
+        }
+    };
+
+    return {
+        history : history
     }
 
 })($);
